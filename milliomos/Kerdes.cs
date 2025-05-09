feba0a24608ed710741e4ce7e5dadfaa29df7e64
Petrik Diak <diak@petrik.hu>
2025-05-09 12:58:33 +0200

    hat igen

---
+++ milliomos/Kerdes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace milliomos
+{
+	internal class Kerdes
+	{
+		private string sorkerdes;
+		private string valasz;
+		private string kategoria;
+
+		public string Sorkerdes { get => sorkerdes; set => sorkerdes = value; }
+		public string Valasz { get => valasz; set => valasz = value; }
+		public string Kategoria { get => kategoria; set => kategoria = value; }
+
+
+	}
+}
