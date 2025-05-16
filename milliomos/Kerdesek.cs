de408d4ea7a6aa538de8366dafa13e3c7f734e53
MAlex07 <molnar.alex@petrik.hu>
2025-05-16 11:41:00 +0200

    lehet jó lesz lehet nem

--- milliomos/Kerdesek.cs
+++ milliomos/Kerdesek.cs
@@ -8,6 +8,29 @@ namespace milliomos
 {
 	internal class Kerdesek
 	{
+        private int nehezseg;
+		string kerdes;
+		List<string> valaszok;
+		string helyesValaz;
+		string kategoria;
 
-	}
+        public Kerdesek(int nehezseg, string kerdes, List<string> valaszok, string helyesValaz, string kategoria)
+        {
+            this.nehezseg = nehezseg;
+            this.kerdes = kerdes;
+            this.valaszok = valaszok;
+            this.helyesValaz = helyesValaz;
+            this.kategoria = kategoria;
+        }
+
+        public Kerdesek(string sor)
+        {
+            var elemek = sor.Split(';');
+            nehezseg = int.Parse(elemek[0]);
+            kerdes = elemek[1];
+            valaszok = new List<string> { elemek[2], elemek[3], elemek[4], elemek[5] };
+            helyesValaz = elemek[6];
+            kategoria = elemek[7];
+        }
+    }
 }
